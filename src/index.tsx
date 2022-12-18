185a116123e5d31196d9b4055697d033f4bdb2f2
AleksNik404 <AleksNik404@gmail.com>
2022-12-18 16:32:44 +0300

    feat: add initial slices redux toolkit and store

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import 'normalize';
+import './index.css';
 
 import App from './App';
+import { store } from './store/store';
+import { Provider } from 'react-redux';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
@@ -10,7 +13,7 @@ if (!rootElement) throw new Error('Failed to find the root element');
 const root = createRoot(rootElement);
 
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
---
+++ src/store/Slices/cartSlice.tsx
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+// interface ICartState {}
+
+const initialState = {};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {},
+});
+
+export const {} = cartSlice.actions;
+
+export default cartSlice.reducer;
